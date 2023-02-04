f826a22a1e571548d8befa3065f925119ec4ae34
toms0910 <psp1070@gmail.com>
2023-02-04 16:36:23 +0900

    Initial commit

---
+++ src/main/java/com/github/shiro8613/barnotificationplugin/BarNotificationPlugin.java
@@ -0,0 +1,17 @@
+package com.github.shiro8613.barnotificationplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BarNotificationPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
